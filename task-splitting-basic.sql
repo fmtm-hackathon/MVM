7a43c829c303cdfe33532493c8da7a5f70380a5e
Prabin Bohara <prabinbohara10@gmail.com>
2023-05-21 14:09:48 +0545

    Sql file added

---
+++ task-splitting-basic.sql
@@ -0,0 +1,22 @@
+WITH points AS (
+  WITH intersected_polys AS (
+    SELECT ST_Intersection(p.geom, a.geom) AS geom
+    FROM islington_polygons p, islington_aoi a
+    WHERE ST_Intersects(p.geom, a.geom)
+    AND (tags->>'building' IS NOT NULL)
+  )
+  SELECT *, ST_Centroid(geom) AS centroid_geom
+  FROM intersected_polys
+),
+clustered_polygons AS (
+  SELECT ST_ClusterKMeans(geom, 19) OVER () AS cid, geom
+  FROM points
+),
+enclosing_polygons AS (
+  SELECT cid, ST_ConvexHull(ST_Collect(geom)) AS cluster_polygon
+  FROM clustered_polygons
+  GROUP BY cid
+)
+
+SELECT cid, cluster_polygon
+FROM enclosing_polygons;
